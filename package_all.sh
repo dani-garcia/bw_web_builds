6eb8d22be6f530cd389f0f0e0827349fd8d80101
Robin Schneider <ypid@riseup.net>
2019-12-25 22:54:30 +0100

    package_web_vault.sh uses bash features -> needs to be called with bash
    This is also what the Shebang says already! I assume this worked before
    because sh was symlinked to bash. You should not rely on this!

--- package_all.sh
+++ package_all.sh
@@ -3,19 +3,19 @@
 export UPLOAD_VAULT="n"
 
 export WEB_REF="v2.5.0"
-sh package_web_vault.sh
+bash package_web_vault.sh
 
 export WEB_REF="v2.6.0"
-sh package_web_vault.sh
+bash package_web_vault.sh
 
 export WEB_REF="v2.6.1"
-sh package_web_vault.sh
+bash package_web_vault.sh
 
 export WEB_REF="v2.7.0"
-sh package_web_vault.sh
+bash package_web_vault.sh
 
 export WEB_REF="v2.7.1"
-sh package_web_vault.sh
+bash package_web_vault.sh
 
 export WEB_REF="v2.8.0"
-sh package_web_vault.sh
+bash package_web_vault.sh
