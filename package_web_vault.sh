784a55854f8bd40f1056622e4c368b2cfb6b0eff
Robin Schneider <ypid@riseup.net>
2019-12-25 23:59:26 +0100

    Fix up script with suggestions from ShellCheck and minor fixes
    * `set -o pipefail -o errexit` exit early on error. This is very useful
      to not overlook errors that might not fully break the build but tamper
      with the end result.
    * Fix `mkdir OUTPUT_FOLDER` fail
    * Rework git checkout stuff to only run needed commands.

--- package_web_vault.sh
+++ package_web_vault.sh
@@ -1,63 +1,65 @@
 #!/bin/bash
+set -o pipefail -o errexit
 
 # Error handling
 handle_error() {
-    read -n1 -r -p "FAILED: line $1, exit code $2. Press any key to exit..." key
+    read -n1 -r -p "FAILED: line $1, exit code $2. Press any key to exit..." _
     exit 1
 }
 trap 'handle_error $LINENO $?' ERR
 
 # Ask for ref if not provided
-if [[ -z $WEB_REF ]]; then
-  read -p "Input a git ref (commit hash, branch name, tag name, 'master'): " input
-  WEB_REF=$input
+if [[ -z "$WEB_REF" ]]; then
+  read -rp "Input a git ref (commit hash, branch name, tag name, 'master'): " input
+  WEB_REF="$input"
 fi
 
 # Ask if the result will be uploaded to github releases
 if [[ -z $UPLOAD_VAULT ]]; then
-  read -p "Upload the result to GitHub Releases? (y/n): " input
-  UPLOAD_VAULT=$input
+  read -rp "Upload the result to GitHub Releases? (y/n): " input
+  UPLOAD_VAULT="$input"
 fi
 
 # If a patch was not provided, try to choose one
 if [[ -z $PATCH_NAME ]]; then
     # If a patch with the same name as the ref exists, use it
-    if [ -f patches/$WEB_REF.patch ]; then
+    if [ -f "patches/$WEB_REF.patch" ]; then
         echo "Patch file found, using that"
-        PATCH_NAME=$WEB_REF.patch
+        PATCH_NAME="$WEB_REF.patch"
     else
         echo "Patch file not found, using latest"
         # If not, use the latest one
-        PATCH_NAME=$(ls patches | sort -V | tail -n1)
+        PATCH_NAME="$(find patches -printf "%f\\n" | sort -V | tail -n1)"
     fi
 fi
 
-echo "Building git ref: " $WEB_REF
-echo "Using patch: " $PATCH_NAME
+echo "Building git ref: $WEB_REF"
+echo "Using patch: $PATCH_NAME"
 
 VAULT_FOLDER=web-vault
 OUTPUT_FOLDER=builds
-OUTPUT_NAME=$OUTPUT_FOLDER/bw_web_$WEB_REF.tar.gz
-OUTPUT_MSG=$OUTPUT_NAME.text
+OUTPUT_NAME="$OUTPUT_FOLDER/bw_web_$WEB_REF.tar.gz"
+OUTPUT_MSG="$OUTPUT_NAME.text"
+
+mkdir -p "$OUTPUT_FOLDER"
 
 # If this is the first time, clone the project
-if [ ! -d $VAULT_FOLDER ]; then
-    git clone https://github.com/bitwarden/web.git $VAULT_FOLDER
-    mkdir OUTPUT_FOLDER
+if [ ! -d "$VAULT_FOLDER" ]; then
+    git clone --recursive https://github.com/bitwarden/web.git "$VAULT_FOLDER"
 fi
 
 cd $VAULT_FOLDER
 
 # Clean
-git checkout .
-git submodule foreach --recursive git checkout .
+git checkout -f
 
 # Update branch
 git fetch --tags
 git pull origin master
 
 # Checkput the branch we want
-git checkout $WEB_REF
+git checkout "$WEB_REF"
+git submodule update --recursive --init
 
 # Update submodule
 npm run sub:update
@@ -67,7 +69,7 @@ npm run sub:update
 ## How to apply patches
 # git apply changes.patch
 
-git apply ../patches/$PATCH_NAME
+git apply "../patches/$PATCH_NAME"
 
 # Build
 npm install
@@ -78,13 +80,13 @@ npm run dist
 
 # Prepare the final archives
 cd build
-tar -czvf ../../$OUTPUT_NAME * --owner=0 --group=0
+tar -czvf "../../$OUTPUT_NAME" ./* --owner=0 --group=0
 
 cd ../..
 
 if [[ $UPLOAD_VAULT =~ ^[Yy]$ ]]
 then
-    sed "s/<VERSION>/$WEB_REF/g" release_template.md > $OUTPUT_MSG
+    sed "s/<VERSION>/$WEB_REF/g" release_template.md > "$OUTPUT_MSG"
     # Install from here: https://hub.github.com/
-    hub release create -o -a $OUTPUT_NAME -F $OUTPUT_MSG $WEB_REF
+    hub release create -o -a "$OUTPUT_NAME" -F "$OUTPUT_MSG $WEB_REF"
 fi
